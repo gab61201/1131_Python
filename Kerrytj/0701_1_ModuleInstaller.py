b0a2cddb35f6921a28c87e676ef85a8b74a1faa9
gab61201 <gab61201@gmail.com>
2024-10-13 03:28:50 +0800

    Delete Kerrytj directory

--- Kerrytj/0701_1_ModuleInstaller.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-subprocess.check_call([sys.executable, "-m", "pip", "install", "playwright"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "clipboard"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
-subprocess.run([sys.executable, '-m', 'playwright', 'install', 'chromium'], check=True)
-subprocess.check_call(["playwright", "install", "chromium"])
-
